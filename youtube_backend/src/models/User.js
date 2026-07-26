7cea3eda7e7b2ce27e0ef3b3c79249f49e68ee25
MohitSingh <mohitsingh540zs@gmail.com>
2026-07-26 23:28:26 +0530

    Created User Schema

---
+++ youtube_backend/src/models/User.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        select: false
+    },
+    avatar: {
+        type: String,
+        default: ""
+    },
+    channel: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Channel",
+        default: null
+    }
+}, {
+    timestamps: true,
+    versionKey: false
+});
+
+export default mongoose.model("User", userSchema);
