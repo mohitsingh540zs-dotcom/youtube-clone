ccd047d31b6af4624b8a8fb30e999361e6eccc8b
MohitSingh <mohitsingh540zs@gmail.com>
2026-07-28 16:29:20 +0530

    Added VideoSchema

---
+++ youtube_backend/src/models/Video.js
@@ -0,0 +1,79 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        maxlength: 150
+    },
+
+    description: {
+        type: String,
+        default: "",
+        trim: true,
+        maxlength: 2000
+    },
+
+    thumbnail: {
+        type: String,
+        default: ""
+    },
+
+    videoUrl: {
+        type: String,
+        required: true
+    },
+
+    duration: {
+        type: Number,
+        default: 0,
+        min: 0
+    },
+
+    category: {
+        type: String,
+        enum: [
+            "Education",
+            "Gaming",
+            "Music",
+            "Sports",
+            "Technology",
+            "Entertainment",
+            "General"
+        ],
+        default: "General"
+    },
+
+    views: {
+        type: Number,
+        min: 0,
+        default: 0
+    },
+
+    likes: {
+        type: Number,
+        min: 0,
+        default: 0
+    },
+
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    channel: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Channel",
+        required: true
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false
+    });
+
+export default mongoose.model("Video", videoSchema);
